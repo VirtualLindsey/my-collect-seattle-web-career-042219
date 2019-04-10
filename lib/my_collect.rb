456378794117322e0c3d2912a8b6073cfd552859
L D <tapircode@gmail.com>
2019-04-10 04:44:48 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,24 @@
 
+def my_collect(c)
 
+  if block_given?
+    result = []
+    i = 0
+  
+    while i < array.length
+      result << yield c[i]
+      i = i + 1
+    end
+    
+    return result
+  
+  else
+    i = 0
+    while i < c.length
+      yield c[i]
+      i += 1
+    end 
+    return c
+  end
+  
+end
